d0fd4d3ef8f9d5a0a3f4299c997442d1bd8aca43
RahulR20 <rahul.goyal@binmile.com>
2022-01-28 01:59:21 +0530

    New test Class has been added (for Checking New Commit)

---
+++ src/test/java/com/scripts/MavenActitime/TestVerify.java
@@ -0,0 +1,12 @@
+package com.scripts.MavenActitime;
+
+import org.testng.annotations.Test;
+
+public class TestVerify {
+	
+	@Test
+	public void TestingNewCommit() {
+		System.out.println("Testing New Commit File");
+	}
+
+}
